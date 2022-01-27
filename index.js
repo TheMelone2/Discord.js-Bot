3f6df82e65ab6901262948cb930cb4140ed86119
TheMelone2 <90137253+TheMelone2@users.noreply.github.com>
2022-01-27 16:22:17 +0100

    Added Description
    A command Description was added.
    Enjoy!

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ client.on("ready", () => {
 
 
   client.user.setActivity(` against the Scammers `, { type: "PLAYING" })
-
+  //Status from the bot, you can use also { type: "LISTENING / COMPETENTING"}
+  //And Streaming like this: , {
+  // type: "STREAMING",
+  // url: "https://www.twitch.tv/monstercat"
+ //  });
 
 });
 client.on('message', message => {
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("Pong :ping_pong: \nMy Pings is " + client.ws.ping + "ms. Thats really fast! ")
   }
-
+//Simple Message
+  
   else if (message.content === `<@${client.user.name}>` || message.content === `<@!${client.user.id}>`) {
     message.channel.send(`My Prefix? Here: ${prefix}`);
-  }
+  }// If the Bot was pinged, he say this
   else if (message.content.startsWith(prefix + "help")) {
     message.channel.send({
       embed: {
@@ -29,7 +34,7 @@ client.on('message', message => {
         timestamp: new Date(),
         footer: {
           text: `${message.guild.name} | YourBot.com`
-        }
+        } //Embed Message
       }
     })
 
